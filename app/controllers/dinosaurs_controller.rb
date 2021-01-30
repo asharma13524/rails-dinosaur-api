2888fdece8f9c1d5f76d9e5056c7a3fb39b2b7ff
asharma13524 <asharma13524@gmail.com>
2021-01-30 12:09:12 -0500

    added create dinosaur route

--- app/controllers/dinosaurs_controller.rb
+++ app/controllers/dinosaurs_controller.rb
@@ -2,4 +2,19 @@ class DinosaursController < ApplicationController
   def index
     render json: Dinosaur.all
   end
+
+  def create
+    dinosaur = Dinosaur.new(dinosaur_params)
+    if dinosaur.save
+      render json: dinosaur, status: :created
+    else
+      render json: dinosaur.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def dinosaur_params
+    params.require(:dinosaur).permit(:name, :species, :diet)
+  end
 end

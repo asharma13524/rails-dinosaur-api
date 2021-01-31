d678cc9e5bd958705ef9434d357ba64eea0ce113
asharma13524 <asharma13524@gmail.com>
2021-01-31 18:41:02 -0500

    added create cage route

--- app/controllers/cages_controller.rb
+++ app/controllers/cages_controller.rb
@@ -5,4 +5,21 @@ class CagesController < ApplicationController
     render json: Cage.all
   end
 
+  # create a new cage
+  def create
+    cage = Cage.new(cage_params)
+
+    if cage.save
+      render json: cage, status: :created
+    else
+      render json: cage.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def cage_params
+    params.require(:cage).permit(:cage_status, :cage_type, :species, :max_dinosaurs)
+  end
+
 end
